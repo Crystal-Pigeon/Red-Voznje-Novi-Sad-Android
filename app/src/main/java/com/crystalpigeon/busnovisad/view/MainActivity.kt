45e5f7bd88d5582d4524f35283e3703a65691faf
saragajic115 <saragajic115@gmail.com>
2019-11-12 18:57:21 +0100

    #BUNS-23 Splash screen created

--- app/src/main/java/com/crystalpigeon/busnovisad/view/MainActivity.kt
+++ app/src/main/java/com/crystalpigeon/busnovisad/view/MainActivity.kt
@@ -7,6 +7,7 @@ import com.crystalpigeon.busnovisad.R
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
