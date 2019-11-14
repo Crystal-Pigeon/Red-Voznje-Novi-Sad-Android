aa72602fbf1947a6d3e14dc5bca433520fb65548
anjagasic <anjagasic1997@gmail.com>
2019-11-14 17:42:32 +0100

    #BUNS-25 implemented basic UI for main screen, added tabs and viewpager

---
+++ app/src/main/java/com/crystalpigeon/busnovisad/view/fragment/MainFragment.kt
@@ -0,0 +1,38 @@
+package com.crystalpigeon.busnovisad.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+import com.crystalpigeon.busnovisad.R
+import com.crystalpigeon.busnovisad.view.MainActivity
+import com.crystalpigeon.busnovisad.view.adapter.PagerAdapter
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController =
+            Navigation.findNavController(activity as MainActivity, R.id.nav_host_fragment)
+        viewpager.adapter = PagerAdapter(childFragmentManager, context!!)
+        tablayout.setupWithViewPager(viewpager)
+
+        fabAddLines.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_addLinesFragment)
+        }
+    }
+}
