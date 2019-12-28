a9010ba46606bb8fe4a4c01c553307afce23692e
nikola.samardzija <nikola.samardzija.5@gmail.com>
2019-12-28 10:01:31 +0100

    Update gradle and kotlin version

--- app/src/main/java/com/crystalpigeon/busnovisad/view/fragment/SettingsFragment.kt
+++ app/src/main/java/com/crystalpigeon/busnovisad/view/fragment/SettingsFragment.kt
@@ -42,7 +42,8 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navController =
-            Navigation.findNavController(activity as MainActivity,
+            Navigation.findNavController(
+                activity as MainActivity,
                 R.id.nav_host_fragment
             )
         (activity as MainActivity).showBackButton()
@@ -55,7 +56,7 @@ class SettingsFragment : Fragment() {
         llSupport.setOnClickListener {
             navController.navigate(R.id.action_settingsFragment_to_supportFragment)
         }
-        if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
             ivSupport.setImageResource(R.drawable.forward_white)
 
         if (sharedPreferences.getString(Const.LANGUAGE, null) == "en")
@@ -64,12 +65,15 @@ class SettingsFragment : Fragment() {
             tvLanguage.text = getString(R.string.serbian)
 
         when {
-            sharedPreferences.getString(Const.THEME, null)=="dark" -> tvTheme.text = getString(R.string.dark)
-            sharedPreferences.getString(Const.THEME, null)=="light" -> tvTheme.text = getString(R.string.light)
-            sharedPreferences.getString(Const.THEME, null)=="default" -> tvTheme.text = getString(R.string.default_theme)
+            sharedPreferences.getString(Const.THEME, null) == "dark" -> tvTheme.text =
+                getString(R.string.dark)
+            sharedPreferences.getString(Const.THEME, null) == "light" -> tvTheme.text =
+                getString(R.string.light)
+            sharedPreferences.getString(Const.THEME, null) == "default" -> tvTheme.text =
+                getString(R.string.default_theme)
         }
-        llLanguage.setOnClickListener{ createAlertDialogForLanguage() }
-        llTheme.setOnClickListener{ createAlertDialogForTheme() }
+        llLanguage.setOnClickListener { createAlertDialogForLanguage() }
+        llTheme.setOnClickListener { createAlertDialogForTheme() }
     }
 
     private fun createAlertDialogForLanguage() {
@@ -95,7 +99,11 @@ class SettingsFragment : Fragment() {
     }
 
     private fun createAlertDialogForTheme() {
-        val values = arrayOf<CharSequence>(getString(R.string.light), getString(R.string.dark), getString(R.string.default_theme))
+        val values = arrayOf<CharSequence>(
+            getString(R.string.light),
+            getString(R.string.dark),
+            getString(R.string.default_theme)
+        )
 
         val builder = AlertDialog.Builder(activity)
 
